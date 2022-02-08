4bff1ad21336fbbbe9900d5f28fe243b4d60faec
Bartosz Klonowski <70535775+BartoszKlonowski@users.noreply.github.com>
2022-02-09 00:33:45 +0100

    Install windows project

---
+++ windows/rnsliderreproapp/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::rnsliderreproapp::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::rnsliderreproapp::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
