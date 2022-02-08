85c529d6a37f42375fe2199007f79b38feeabfa1
Bartosz Klonowski <70535775+BartoszKlonowski@users.noreply.github.com>
2022-02-09 00:22:40 +0100

    Create empty react-native project with tooling

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    "react-native/react-native": true
+  },
+  parser: "babel-eslint",
+  plugins: [
+    "react",
+    "react-native"
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+        jsx: true
+    }
+  }
+};
