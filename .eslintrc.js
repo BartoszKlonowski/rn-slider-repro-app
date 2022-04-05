24873b53d646465d51eda09504844e35c344914a
Bartosz Klonowski <70535775+BartoszKlonowski@users.noreply.github.com>
2022-04-05 22:59:56 +0200

    Install and configure @callstack/eslint-config
    The new ESLint configuration is to replace the the existing raw
    configuration, and to enable better formatting standard.
    This commit also adds the new plugin usage to the CI configuration.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,3 @@
 module.exports = {
-  extends: ["plugin:react-native/all"],
-  env: {
-    "react-native/react-native": true
-  },
-  parser: "babel-eslint",
-  plugins: [
-    "react",
-    "react-native"
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-        jsx: true
-    }
-  }
+  extends: ["@callstack"],
 };
