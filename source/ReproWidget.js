ff732a0c3a446d74dbce6c3aa5300d4c6b217880
Bartosz Klonowski <70535775+BartoszKlonowski@users.noreply.github.com>
2022-04-07 01:58:08 +0200

    Use react-query to show the state of issue

--- source/ReproWidget.js
+++ source/ReproWidget.js
@@ -1,32 +1,54 @@
 import React from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { useQuery } from "react-query";
 
-export class ReproWidget extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      navigation: props.navigation,
-      issueReproExample: props.issueReproExample,
-    };
-  }
+export const ReproWidget = ({ navigation, issueNumber }) => {
+  const { isLoading, data } = useQuery(issueNumber, () =>
+    fetch(
+      `https://api.github.com/repos/callstack/react-native-slider/issues/${issueNumber}`
+    ).then((res) => res.json())
+  );
 
-  render() {
+  if (isLoading) {
     return (
-      <TouchableOpacity style={styles.reproWidget} onPress={() => {
-        this.state.navigation.navigate(
-          String(this.state.issueReproExample),
-          { issue: this.state.issueReproExample }
-        );
-      }}>
-      <View>
-        <Text>Navigate to: {this.state.issueReproExample}</Text>
+      <View style={styles.reproWidget}>
+        <Text>Still loading the data...</Text>
       </View>
-      </TouchableOpacity>
     );
   }
-}
+
+  return (
+    <TouchableOpacity
+      accessibilityRole="button"
+      style={[
+        styles.reproWidget,
+        { borderLeftColor: data.state === "closed" ? "#8250DF" : "#1A7F37" },
+      ]}
+      onPress={() => {
+        navigation.navigate(String(issueNumber), { issue: issueNumber });
+      }}
+    >
+      <View>
+        <Text style={styles.issueHeader}>{issueNumber}</Text>
+        <Text style={styles.issueBrief}>Brief info about the issue</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
   reproWidget: {
-  }
-})
+    margin: 20,
+    borderBottomWidth: 1,
+    borderTopWidth: 1,
+    borderLeftWidth: 10,
+    borderLeftColor: "blue",
+  },
+  issueHeader: {
+    fontWeight: "bold",
+    margin: 5,
+  },
+  issueBrief: {
+    margin: 5,
+  },
+});
--- source/Screens/HomeScreen.js
+++ source/Screens/HomeScreen.js
@@ -1,19 +1,28 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { QueryClient, QueryClientProvider } from "react-query";
 import screens from "../ReproExamples/Issues";
 import { ReproWidget } from "../ReproWidget";
 
+const queryClient = new QueryClient();
+
 export function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-      {screens.map((issue) => (
-        <ReproWidget
-          navigation={navigation}
-          issueReproExample={issue.name}
-          key={issue.name}
-        />
-      ))}
-    </View>
+    <QueryClientProvider client={queryClient}>
+      <View
+        style={{ flex: 1, alignItems: "stretch", justifyContent: "center" }}
+      >
+        <Text style={{ margin: 20 }}>
+          Select an issue to see the repro example:
+        </Text>
+        {screens.map((issue) => (
+          <ReproWidget
+            navigation={navigation}
+            issueNumber={issue.name}
+            key={issue.name}
+          />
+        ))}
+      </View>
+    </QueryClientProvider>
   );
 }
