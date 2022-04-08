6c530f7703622ff96498a579032309baf5f6127e
Bartosz Klonowski <70535775+BartoszKlonowski@users.noreply.github.com>
2022-04-08 18:40:55 +0200

    Polish the DetailsScreen

--- source/Screens/DetailsScreen.js
+++ source/Screens/DetailsScreen.js
@@ -1,16 +1,32 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Text, Linking } from "react-native";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryClient = new QueryClient();
 
-export function DetailsScreen({ navigation }) {
+export function DetailsScreen({ route }) {
   return (
     <QueryClientProvider client={queryClient}>
       <View style={styles.mainLayout}>
-        <View style={styles.header}></View>
+        <View style={styles.header}>
+          <Text style={styles.titleText}>{route.params.title}</Text>
+          <Text style={styles.headerText}>Issue: {route.params.issue}</Text>
+          <Text style={styles.headerText}>
+            Platform: {route.params.platform}
+          </Text>
+          <Text style={styles.headerText}>
+            Created: {new Date(route.params.dateCreated).toDateString()}
+          </Text>
+        </View>
         <View style={styles.reproExample}></View>
-        <View style={styles.footer}></View>
+        <View style={styles.footer}>
+          <Text
+            style={styles.issueLinkText}
+            onPress={() => Linking.openURL(route.params.url)}
+          >
+            Go to issue
+          </Text>
+        </View>
       </View>
     </QueryClientProvider>
   );
@@ -19,16 +35,36 @@ export function DetailsScreen({ navigation }) {
 const styles = StyleSheet.create({
   mainLayout: {
     flex: 1,
-    alignItems: "stretch",
-    justifyContent: "center",
   },
   header: {
+    justifyContent: "space-around",
     margin: 20,
+    borderBottomWidth: 1,
+  },
+  headerText: {
+    fontSize: 15,
+    fontWeight: "600",
+    fontFamily: "Roboto",
+    color: "black",
+  },
+  titleText: {
+    fontSize: 18,
+    fontWeight: "800",
+    fontFamily: "Roboto",
+    color: "black",
+    alignSelf: "center",
   },
   footer: {
     margin: 20,
+    alignItems: "center",
+    borderTopWidth: 1,
   },
   reproExample: {
+    height: "60%",
     margin: 20,
+  },
+  issueLinkText: {
+    color: "blue",
+    textDecorationLine: "underline",
   },
 });
