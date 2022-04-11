37a53122a7b1779024c3ca0e9dd352961fd9f961
BartoszKlonowski <Bartosz.Klonowski@callstack.com>
2022-04-11 23:04:35 +0200

    Add Issue250 to list of repros

--- source/ReproExamples/Issues.js
+++ source/ReproExamples/Issues.js
@@ -1,5 +1,6 @@
+import { Issue250 } from "./Issue250";
 import { Issue366 } from "./Issue366";
 
-const screens = [{ name: "366", component: Issue366 }];
+const screens = [{ name: "366", component: Issue366 }, {name: "250", component: Issue250}];
 
 export default screens;
