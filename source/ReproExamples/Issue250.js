ceae7fdbbffc0382ed3d5895be6ca0f4b2a8ea66
Bartosz Klonowski <70535775+BartoszKlonowski@users.noreply.github.com>
2022-04-13 12:31:43 +0200

    Add Issue 250 (#11)
    * Implement basic repro example
    
    * Add Issue250 to list of repros
    
    * Fix linting
    
    * Remove Roboto in favor of Arial

---
+++ source/ReproExamples/Issue250.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { View, StyleSheet, Text } from "react-native";
+import Slider from "@react-native-community/slider";
+
+export function Issue250() {
+  const [locationMaxWaitTime, setLocationMaxWaitTime] = useState(0 | null);
+
+  const handleAccessibleChangeMaxWaitTime = (increment) => {
+    const times = [5, 15, 30, 45, null];
+    const maxWaitTime = 45;
+
+    let index = times.indexOf(maxWaitTime);
+
+    if (index === -1) {
+      index = 4;
+    }
+    if (increment) {
+      if (index === times.length - 1 || !maxWaitTime) {
+        return;
+      }
+      setLocationMaxWaitTime(times[index + 1]);
+      console.log("locationMaxWaitTime: ", locationMaxWaitTime);
+    } else {
+      if (index === 0) {
+        return;
+      }
+      setLocationMaxWaitTime(times[index - 1]);
+      console.log("locationMaxWaitTime: ", locationMaxWaitTime);
+    }
+  };
+
+  return (
+    <View style={styles.mainLayout}>
+      <Slider
+        minimumValue={0}
+        maximumValue={100}
+        value={0}
+        accessibilityActions={[{ name: "increment" }, { name: "decrement" }]}
+        onAccessibilityAction={(event) => {
+          switch (event.nativeEvent.actionName) {
+            case "increment":
+              handleAccessibleChangeMaxWaitTime(true);
+              break;
+            case "decrement":
+              handleAccessibleChangeMaxWaitTime(false);
+              break;
+            default:
+              break;
+          }
+        }}
+      />
+      <Text>locationMaxWaitTime: {locationMaxWaitTime}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  mainLayout: {
+    flex: 1,
+    alignItems: "stretch",
+    justifyContent: "center",
+  },
+});
