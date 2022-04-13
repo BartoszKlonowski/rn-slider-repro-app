9613896222f1025fad86e85c75c5fdb41061958e
BartoszKlonowski <Bartosz.Klonowski@callstack.com>
2022-04-13 14:00:30 +0200

    Remove comments from the ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,19 +15,16 @@ module.exports = {
   },
   overrides: [
     {
-      files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+      files: ['*.ts', '*.tsx'],
       parser: '@typescript-eslint/parser',
       plugins: ["@typescript-eslint"],
-      // As mentioned in the comments, you should extend TypeScript plugins here,
-      // instead of extending them outside the `overrides`.
-      // If you don't want to extend any rules, you don't need an `extends` attribute.
       extends: [
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
       ],
 
       parserOptions: {
-        project: ['./tsconfig.json'], // Specify it only for TypeScript files
+        project: ['./tsconfig.json'],
       },
     },
   ],
