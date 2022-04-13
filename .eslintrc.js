4cb7aeff24bcb5a6903d08e0875429ede43ee14a
BartoszKlonowski <Bartosz.Klonowski@callstack.com>
2022-04-13 13:30:09 +0200

    Configure TypeScript for the Issue321 for ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,23 @@ module.exports = {
     ecmaFeatures: {
         jsx: true
     }
-  }
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+      parser: '@typescript-eslint/parser',
+      plugins: ["@typescript-eslint"],
+      // As mentioned in the comments, you should extend TypeScript plugins here,
+      // instead of extending them outside the `overrides`.
+      // If you don't want to extend any rules, you don't need an `extends` attribute.
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+
+      parserOptions: {
+        project: ['./tsconfig.json'], // Specify it only for TypeScript files
+      },
+    },
+  ],
 };
