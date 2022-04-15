c29813dbcf38fb8c7903a858c8a5881f91f48e52
Bartosz Klonowski <70535775+BartoszKlonowski@users.noreply.github.com>
2022-04-15 19:40:08 +0200

    Add Issue 96 (#13)
    * Implement Issue 96 according to repro
    
    * Add Issue 96 to the issues list
    
    * Display the value of each Slider to see the position and offset
    
    * Fix linting errors

---
+++ source/ReproExamples/Issue96.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { View, StyleSheet, Text } from "react-native";
+import Slider from "@react-native-community/slider";
+
+export function Issue96() {
+  const [defaultValue, setDefaultValue] = useState(0);
+  const [orangeValue, setOrangeValue] = useState(0);
+
+  return (
+    <View style={styles.mainLayout}>
+      <Text style={styles.sliderIntroText}>
+        Slider with default color of Thumb:
+      </Text>
+      <Slider
+        minimumValue={0}
+        maximumValue={100}
+        step={1}
+        value={defaultValue}
+        onValueChange={setDefaultValue}
+      />
+      <Text>Value: {defaultValue}</Text>
+      <Text style={styles.sliderIntroText}>
+        Slider with custom color of Thumb:
+      </Text>
+      <Slider
+        minimumValue={0}
+        maximumValue={100}
+        step={1}
+        value={orangeValue}
+        onValueChange={setOrangeValue}
+        thumbTintColor={"orange"}
+      />
+      <Text>Value: {orangeValue}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  mainLayout: {
+    flex: 1,
+    alignItems: "stretch",
+    justifyContent: "center",
+  },
+  sliderIntroText: {
+    fontSize: 20,
+    margin: 5,
+  },
+});
