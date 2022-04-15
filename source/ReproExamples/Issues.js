00f1a9179ff4618b6c96c2c3c780b41bfa3b08b9
BartoszKlonowski <Bartosz.Klonowski@callstack.com>
2022-04-15 17:14:02 +0200

    Add Issue 96 to the issues list

--- source/ReproExamples/Issues.js
+++ source/ReproExamples/Issues.js
@@ -1,11 +1,13 @@
 import { Issue250 } from "./Issue250";
 import { Issue321 } from "./Issue321";
 import { Issue366 } from "./Issue366";
+import { Issue96 } from "./Issue96";
 
 const screens = [
   { name: "366", component: Issue366 },
   { name: "250", component: Issue250 },
   { name: "321", component: Issue321 },
+  { name: "96", component: Issue96 },
 ];
 
 export default screens;
