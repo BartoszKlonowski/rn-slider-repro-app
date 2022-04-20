cba329bfd90085e5f02d018df89dcbd10e7f0802
Bartosz Klonowski <70535775+BartoszKlonowski@users.noreply.github.com>
2022-04-20 21:05:53 +0200

    Add Issue 370 (#14)
    * Implement basic example of Issue 370
    
    * Adjust example to the scenario
    
    * Lint

--- source/ReproExamples/Issues.js
+++ source/ReproExamples/Issues.js
@@ -2,6 +2,7 @@ import { Issue250 } from "./Issue250";
 import { Issue321 } from "./Issue321";
 import { Issue346 } from "./Issue346";
 import { Issue366 } from "./Issue366";
+import { Issue370 } from "./Issue370";
 import { Issue96 } from "./Issue96";
 
 const screens = [
@@ -9,6 +10,7 @@ const screens = [
   { name: "250", component: Issue250 },
   { name: "321", component: Issue321 },
   { name: "96", component: Issue96 },
+  { name: "370", component: Issue370 },
   { name: "346", component: Issue346 },
 ];
 
