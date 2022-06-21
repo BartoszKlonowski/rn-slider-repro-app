c587e40ad63fd20699eb7c1b36f7cce6511836a6
BartoszKlonowski <Bartosz.Klonowski@callstack.com>
2022-06-21 03:59:21 +0200

    Include controlled value Slider in the repro

--- source/ReproExamples/Issue390.js
+++ source/ReproExamples/Issue390.js
@@ -1,13 +1,25 @@
 import Slider from "@react-native-community/slider";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 export function Issue390() {
   const [value, setValue] = useState(0);
+  const [controlledValue, setControlledValue] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {
+      setControlledValue((v) => v + 1);
+    }, 500);
+  }, []);
 
   const onChange = (value) => {
     setValue(value);
     console.log("onChange.value: ", value);
+  };
+
+  const interrupt = (value) => {
+    setControlledValue(value);
+    console.log("interrupt controlledValue to be: ", value);
   };
 
   return (
@@ -21,6 +33,15 @@ export function Issue390() {
         tapToSeek
       />
       <Text>Current value: {value}</Text>
+      <Slider
+        minimumValue={0}
+        maximumValue={100}
+        value={controlledValue}
+        onValueChange={interrupt}
+        step={20}
+        tapToSeek
+      />
+      <Text>Current controlled value: {controlledValue}</Text>
     </View>
   );
 }
