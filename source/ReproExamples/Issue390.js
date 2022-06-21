7369076198ab21a8695e1ad6e696cc2ba6c5f928
BartoszKlonowski <Bartosz.Klonowski@callstack.com>
2022-06-21 03:02:23 +0200

    Include missing details in the repro example

--- source/ReproExamples/Issue390.js
+++ source/ReproExamples/Issue390.js
@@ -1,16 +1,33 @@
 import Slider from '@react-native-community/slider';
 import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 export function Issue390() {
 	const [value, setValue] = useState(0);
 
-	return (
-		<Slider
-			value={value}
-			step={1}
-			minimumValue={0}
-			maximumValue={5}
-			onValueChange={setValue}
-		/>
-	);
-};
+  const onChange = (value) => {
+    setValue(value);
+    console.log("onChange.value: ", value);
+  };
+
+  return (
+    <View style={styles.mainLayout}>
+      <Slider
+        onValueChange={onChange}
+        minimumValue={0}
+        maximumValue={100}
+        value={value}
+        step={20}
+      />
+      <Text>Current value: {value}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  mainLayout: {
+    flex: 1,
+    alignItems: "stretch",
+    justifyContent: "center",
+  },
+});
