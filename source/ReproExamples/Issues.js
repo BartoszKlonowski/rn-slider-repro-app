606320e787569a3b0944024888394eb9f4bbe89c
BartoszKlonowski <Bartosz.Klonowski@callstack.com>
2022-06-23 22:43:00 +0200

    Add Issue391 to the repro app

--- source/ReproExamples/Issues.js
+++ source/ReproExamples/Issues.js
@@ -6,6 +6,7 @@ import { Issue346 } from "./Issue346";
 import { Issue366 } from "./Issue366";
 import { Issue370 } from "./Issue370";
 import { Issue377 } from "./Issue377";
+import { Issue391 } from "./Issue391";
 import { Issue96 } from "./Issue96";
 
 const screens = [
@@ -18,6 +19,7 @@ const screens = [
   { name: "345", component: Issue345 },
   { name: "227", component: Issue227 },
   { name: "377", component: Issue377 },
+  {name: "391", component: Issue391 },
 ];
 
 export default screens;
