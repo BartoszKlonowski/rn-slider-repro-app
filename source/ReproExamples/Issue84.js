20c63994aec500cebc63e8072538d14cf6aa0536
BartoszKlonowski <Bartosz.Klonowski@callstack.com>
2023-01-18 13:10:12 +0100

    Implement basic example of issue 84

---
+++ source/ReproExamples/Issue84.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { View, StyleSheet, Text } from "react-native";
+import Slider from "@react-native-community/slider";
+
+export function Issue84() {
+  return (
+    <View style={styles.mainLayout}>
+      <Text style={styles.sliderIntroText}>
+        Slider with same tint colors:
+      </Text>
+      <Slider
+        minimumValue={0}
+        maximumValue={100}
+        step={1}
+        value={50}
+        maximumTrackTintColor="#ABCDEF"
+        minimumTrackTintColor="#ABCDEF"
+      />
+      <Text style={styles.sliderIntroText}>
+        Slider with different tint colors:
+      </Text>
+      <Slider
+        minimumValue={0}
+        maximumValue={100}
+        step={1}
+        value={50}
+        maximumTrackTintColor="#FEDCBA"
+        minimumTrackTintColor="#012345"
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  mainLayout: {
+    flex: 1,
+    alignItems: "stretch",
+    justifyContent: "center",
+  },
+  sliderIntroText: {
+    fontSize: 20,
+    margin: 5,
+  },
+});
