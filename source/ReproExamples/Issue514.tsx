f5987c2866d505e37f3141f4439bcaa2327d5d7f
BartoszKlonowski <Bartosz.Klonowski@callstack.com>
2023-06-23 15:28:38 +0200

    Fix typing and linting

--- source/ReproExamples/Issue514.tsx
+++ source/ReproExamples/Issue514.tsx
@@ -1,47 +1,53 @@
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text } from "react-native";
+import React, { Fragment } from "react";
 
-import Slider from '@react-native-community/slider';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-import { useState } from 'react';
+import Slider from "@react-native-community/slider";
+import { useState } from "react";
 
 interface TrackMarksProps {
   isTrue: boolean;
 }
 
-
 export const Issue514 = () => {
-    const [value, setValue] = useState(0);
-    const options = Array.from({ length: 7 / 1 + 1 }, (_, index) => index * 1);
-    return(
-      <View>
-        <Slider
-          minimumValue={0}
-          maximumValue={10}
-          step={1}
-          tapToSeek
-          onValueChange={setValue}
-          minimumTrackTintColor={"#11FF11"}
-          maximumTrackTintColor={"#11FF11"}
-          style={{ zIndex: 1 }}
-          thumbImage={require('../ReproAssets/Issue346_ThumbImage.png')}
-        />
-        <View
-          pointerEvents="none"
-          style={{ justifyContent: 'space-between', flexDirection: 'row', top: -25, zIndex: 2 }}
-        >
-          {options.map((i, index) => {
-            return (
-              <View style={{ alignItems: 'center' }}>
-                <SliderTrackMark key={index} isTrue={value === i} />
-                <View style={{ marginTop: 20, alignItems: 'center' }}><Text>{i}</Text></View>
+  const [value, setValue] = useState(0);
+  const options = Array.from({ length: 7 / 1 + 1 }, (_, index) => index * 1);
+  return (
+    <View>
+      <Slider
+        minimumValue={0}
+        maximumValue={7}
+        step={1}
+        tapToSeek
+        onValueChange={setValue}
+        minimumTrackTintColor={"#11FF11"}
+        maximumTrackTintColor={"#11FF11"}
+        style={{ zIndex: 1 }}
+        // eslint-disable-next-line
+        thumbImage={require("../ReproAssets/Issue346_ThumbImage.png")}
+      />
+      <View
+        pointerEvents="none"
+        style={{
+          justifyContent: "space-between",
+          flexDirection: "row",
+          top: -25,
+          zIndex: 2,
+        }}
+      >
+        {options.map((i, index) => {
+          return (
+            <Fragment key={index}>
+              <View style={{ alignItems: "center" }}>
+                <SliderTrackMark key={`${index}-SliderTrackMark`} isTrue={value === i} />
+                <Paragraph i={i} key={`${index}-Paragraph`} />
               </View>
-            );
-          })}
-        </View>
+            </Fragment>
+          );
+        })}
       </View>
-    );
-}
-
+    </View>
+  );
+};
 
 function SliderTrackMark({ isTrue }: TrackMarksProps) {
   return isTrue ? (
@@ -54,22 +60,31 @@ function SliderTrackMark({ isTrue }: TrackMarksProps) {
     </View>
   );
 }
+
+function Paragraph({ i }: { i: number }) {
+  return (
+    <View style={{ marginTop: 20, alignItems: "center" }}>
+      <Text>{i}</Text>
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
   outer: {
     width: 10,
     height: 10,
     borderRadius: 10,
     backgroundColor: "#11FF11",
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   outerTrue: {
     width: 10,
     height: 10,
     borderRadius: 10,
     backgroundColor: "#0F0FFF",
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   inner: {
     width: 7,
@@ -84,4 +99,3 @@ const styles = StyleSheet.create({
     backgroundColor: "#0F0FFF",
   },
 });
-
