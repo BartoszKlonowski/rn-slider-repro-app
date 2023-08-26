ac8466d0e154da7bc8c2de61c32f74b43f7af545
BartoszKlonowski <Bartosz.Klonowski@callstack.com>
2023-08-26 03:36:30 +0200

    Implement all examples of usage of CustomizedSlider

--- source/ReproExamples/Issue514.tsx
+++ source/ReproExamples/Issue514.tsx
@@ -1,17 +1,30 @@
-import { View, StyleSheet, Text, Image, ImageURISource } from "react-native";
-import React, { Fragment } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  Image,
+  ImageURISource,
+  Platform,
+} from "react-native";
+import React, { FC, Fragment } from "react";
 
 import Slider, { SliderProps } from "@react-native-community/slider";
 import { useState } from "react";
 
 interface TrackMarksProps {
   isTrue: boolean;
-  image?: boolean;
   thumbImage?: ImageURISource;
+  StepMarker?: FC<MarkerProps> | boolean;
+}
+
+interface MarkerProps {
+  stepMarked: boolean;
 }
 
 interface CustomSliderProps extends SliderProps {
   image?: ImageURISource;
+  stepMarker?: FC<MarkerProps> | boolean;
+  renderStepNumber?: boolean;
 }
 
 const CustomizedSlider = (props: CustomSliderProps) => {
@@ -29,7 +42,7 @@ const CustomizedSlider = (props: CustomSliderProps) => {
       onLayout={(event) => {
         setWidth(event.nativeEvent.layout.width);
       }}
-      style={[{ zIndex: 1, width: "100%", height: "100%" }, props.style]}
+      style={[{ zIndex: 1, width: "100%" }, props.style]}
     >
       <Slider
         {...props}
@@ -40,42 +53,55 @@ const CustomizedSlider = (props: CustomSliderProps) => {
           }
         }}
         style={{ zIndex: 1, width: width }}
-        thumbTintColor={props.thumbImage ? "transparent" : props.thumbTintColor}
+        thumbTintColor={
+          props.thumbImage && !!props.stepMarker
+            ? "transparent"
+            : props.thumbTintColor
+        }
         // eslint-disable-next-line
-        thumbImage={undefined}
+        thumbImage={props.stepMarker ? undefined : props.thumbImage}
       />
-      <View
-        pointerEvents="none"
-        style={{
-          width: width - options.length,
-          flexDirection: "row",
-          top: -25,
-          zIndex: 2,
-        }}
-      >
-        {options.map((i, index) => {
-          return (
-            <Fragment key={index}>
-              <View
-                style={{ alignItems: "center", width: width / options.length }}
-              >
-                <SliderTrackMark
-                  key={`${index}-SliderTrackMark`}
-                  isTrue={currentValue === i}
-                  image={currentValue === i}
-                  thumbImage={props.thumbImage}
-                />
-                <Paragraph i={i} key={`${index}-Paragraph`} />
-              </View>
-            </Fragment>
-          );
-        })}
-      </View>
+      {props.stepMarker || !!props.renderStepNumber ? (
+        <View
+          pointerEvents="none"
+          style={{
+            width: width - options.length,
+            flexDirection: "row",
+            top: -25,
+            zIndex: 2,
+          }}
+        >
+          {options.map((i, index) => {
+            return (
+              <Fragment key={index}>
+                <View
+                  style={{
+                    alignItems: "center",
+                    width: width / options.length,
+                  }}
+                >
+                  <SliderTrackMark
+                    key={`${index}-SliderTrackMark`}
+                    isTrue={currentValue === i}
+                    thumbImage={props.thumbImage}
+                    StepMarker={props.stepMarker}
+                  />
+                  {props.renderStepNumber ? (
+                    <Paragraph i={i} key={`${index}-Paragraph`} />
+                  ) : null}
+                </View>
+              </Fragment>
+            );
+          })}
+        </View>
+      ) : null}
     </View>
   );
 };
 
 export const Issue514 = () => {
+  // eslint-disable-next-line
+  const image = require("../ReproAssets/Issue346_ThumbImage.png");
   return (
     <View
       style={{
@@ -83,6 +109,7 @@ export const Issue514 = () => {
         alignItems: "center",
       }}
     >
+      <Text style={{ marginTop: 20 }}>Slider just like before</Text>
       <CustomizedSlider
         minimumValue={0}
         maximumValue={10}
@@ -91,23 +118,88 @@ export const Issue514 = () => {
         minimumTrackTintColor={"#11FF11"}
         maximumTrackTintColor={"#11FF11"}
       />
+      <Text style={{ marginTop: 20 }}>
+        Slider just like before, but with image
+      </Text>
+      <CustomizedSlider
+        minimumValue={0}
+        maximumValue={10}
+        step={1}
+        // eslint-disable-next-line
+        thumbImage={image}
+        tapToSeek
+        minimumTrackTintColor={"#11FF11"}
+        maximumTrackTintColor={"#11FF11"}
+      />
+      <Text style={{ marginTop: 20 }}>
+        Slider with default steps indicator and numbers
+      </Text>
+      <CustomizedSlider
+        minimumValue={0}
+        maximumValue={10}
+        step={1}
+        tapToSeek
+        stepMarker
+        renderStepNumber
+        minimumTrackTintColor={"#123456"}
+        maximumTrackTintColor={"#123456"}
+      />
+      <Text style={{ marginTop: 20 }}>
+        Slider with customized indicator and no numbers
+      </Text>
+      <CustomizedSlider
+        minimumValue={0}
+        maximumValue={10}
+        step={1}
+        tapToSeek
+        stepMarker={(isMarked) => {
+          return isMarked ? (
+            <View style={styles.outerTrue}>
+              <View style={styles.innerTrue} />
+            </View>
+          ) : (
+            <View style={styles.outer}>
+              <View style={styles.inner} />
+            </View>
+          );
+        }}
+        minimumTrackTintColor={"#654321"}
+        maximumTrackTintColor={"#654321"}
+      />
+      <Text style={{ marginTop: 20 }}>
+        Slider with image as a thumb and no numbers
+      </Text>
+      <CustomizedSlider
+        minimumValue={0}
+        maximumValue={10}
+        step={1}
+        tapToSeek
+        stepMarker
+        // eslint-disable-next-line
+        thumbImage={image}
+        minimumTrackTintColor={"#654321"}
+        maximumTrackTintColor={"#654321"}
+      />
     </View>
   );
 };
 
-function SliderTrackMark({ isTrue, image, thumbImage }: TrackMarksProps) {
+function SliderTrackMark({ isTrue, thumbImage, StepMarker }: TrackMarksProps) {
+  if (typeof StepMarker !== "boolean") {
+    return <StepMarker stepMarked={isTrue} />;
+  }
   return isTrue ? (
-    <View style={image ? styles.outerTrue : styles.outerImage}>
-      {image ? (
-        <Image source={thumbImage} accessibilityIgnoresInvertColors />
+    <>
+      {thumbImage ? (
+        <View style={styles.outerTrue}>
+          <Image source={thumbImage} accessibilityIgnoresInvertColors />
+        </View>
       ) : (
         <View style={styles.innerTrue} />
       )}
-    </View>
+    </>
   ) : (
-    <View style={styles.outer}>
-      <View style={styles.inner} />
-    </View>
+    <View style={styles.inner} />
   );
 }
 
@@ -133,10 +225,6 @@ const styles = StyleSheet.create({
     height: 10,
     borderRadius: 10,
     backgroundColor: "#0F0FFF",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  outerImage: {
     justifyContent: "center",
     alignItems: "center",
   },
